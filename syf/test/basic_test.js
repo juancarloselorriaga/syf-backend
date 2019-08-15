8d8976ead3268c352b59a630709b196905b403ec
David H <iq.davidh@gmail.com>
2019-08-15 11:42:52 -0500

    Agregado elementos para poder realizar pruebas unitarias, agregue solo una prueba y modificacion al pack con las nuevas dependencias si corres npm run test, ya esta funcionando el primer test (dummy)

---
+++ syf/test/basic_test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * Created by David on 18/07/2019.
+ */
+
+const assert = require('assert');
+
+
+describe('probar el funcionamiento de unit test con mocha', function () {
+  it('debe retornar solo true', function () {
+    assert(true, "Deberia ser true - test dummy");
+  });
+});
+
